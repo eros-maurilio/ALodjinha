7d75bb8759698c93e9135e240cb173865122e2aa
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-13 22:51:48 -0300

    Switch colors name and create tabcontrollet types

--- App/ALodjinha/Sources/Extensions/UIColor+Extension.swift
+++ App/ALodjinha/Sources/Extensions/UIColor+Extension.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIColor {
-        static let purple = UIColor(red: 245/255, green: 245/255, blue: 244/255, alpha: 255/255)
-        static let beige = UIColor(red: 99/255, green: 66/255, blue: 140/255, alpha: 255/255)
+        static let beige = UIColor(red: 245/255, green: 245/255, blue: 244/255, alpha: 255/255)
+        static let purple = UIColor(red: 99/255, green: 66/255, blue: 140/255, alpha: 255/255)
         static let white = UIColor(red: 252/255, green: 252/255, blue: 252/255, alpha: 255/255)
 }
---
+++ App/ALodjinha/Sources/Scenes/TabBarController/TabControllerType.swift
@@ -0,0 +1,8 @@
+//
+//  TabControllerType.swift
+//  ALodjinha
+//
+//  Created by Eros Maurilio on 13/01/22.
+//
+
+import Foundation
