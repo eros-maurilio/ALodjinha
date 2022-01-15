7f1389469393800c94843575df6c9ec30409bfb6
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-14 21:20:52 -0300

    Adjust page indicator to respond to cell index

--- App/ALodjinha/Sources/Scenes/Home/HomeViewController.swift
+++ App/ALodjinha/Sources/Scenes/Home/HomeViewController.swift
@@ -2,7 +2,10 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet private weak var bannerCollection: UICollectionView!
-    @IBOutlet weak var layout: UICollectionViewFlowLayout!
+    @IBOutlet private weak var layout: UICollectionViewFlowLayout!
+    @IBOutlet private weak var pageControl: UIPageControl!
+    
+    private let numOfItems = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,7 @@ private extension HomeViewController {
     
     func registerCell(for: UICollectionView) {
         bannerCollection.dataSource = self
+        bannerCollection.delegate = self
         bannerCollection.register(BannerViewCell.self)
     }
     
@@ -39,16 +43,41 @@ private extension HomeViewController {
     }
     
     func configureView() {
-        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 200)
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: bannerCollection.bounds.size.height)
+        pageControl.numberOfPages = numOfItems
+    }
+    
+    func getCurrentCellIndex() -> Int {
+        let visibleRect = CGRect(origin: bannerCollection.contentOffset, size: bannerCollection.bounds.size)
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        if let visibleIndexPath = bannerCollection.indexPathForItem(at: visiblePoint) {
+            return visibleIndexPath.row
+        }
+        
+        return pageControl.currentPage
     }
 }
 
 extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        3
+        numOfItems
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         collectionCell(collectionView, at: indexPath, forACellDTO: BannerCellDTO(image: UIImage(named: "axlPlaceHolder")!))
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl.currentPage = getCurrentCellIndex()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        pageControl.currentPage = getCurrentCellIndex()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = getCurrentCellIndex()
     }
 }
