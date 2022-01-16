e4ed1e328d54ab06247ba168bd4d7e66a040556a
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-15 21:13:04 -0300

    Add space

--- App/ALodjinha/Sources/Extensions/UIView+Extension.swift
+++ App/ALodjinha/Sources/Extensions/UIView+Extension.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 extension UIView {
+    
     static func loadFromNib() -> Self {
         let bundle = Bundle(for: self)
         let nib = UINib(nibName: String(describing: self), bundle: bundle)
