e3ad9406463a83112350c30e50897ea197be24e1
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-18 20:12:34 -0300

    Add new Model arguments

--- App/ALodjinha/Sources/Common/Model/DataModel.swift
+++ App/ALodjinha/Sources/Common/Model/DataModel.swift
@@ -1,6 +1,16 @@
 struct DataModel: Decodable {
     let id: Int
+    let nome: String?
     let urlImagem: String
-    let linkUrl: String?
     let descricao: String?
+    let precoDe: Float?
+    let precoPor: Float?
+    let categoria: Categoria?
+    let linkUrl: String?
+}
+
+struct Categoria: Decodable {
+    let id: Int
+    let descricao: String
+    let urlImagem: String
 }
