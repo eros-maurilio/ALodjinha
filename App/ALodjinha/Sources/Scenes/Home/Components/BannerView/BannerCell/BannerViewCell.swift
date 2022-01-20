69b98df5d34af443d2129411641bd214cca808fa
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-20 19:34:52 -0300

    Add Activity indicator to banner cell

--- App/ALodjinha/Sources/Scenes/Home/Components/BannerView/BannerCell/BannerViewCell.swift
+++ App/ALodjinha/Sources/Scenes/Home/Components/BannerView/BannerCell/BannerViewCell.swift
@@ -8,12 +8,40 @@ class BannerViewCell: UICollectionViewCell {
    
     // MARK: - IBOutlets
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet private weak var bannerImage: ImageCacherView!
     
     // MARK: - Public Method
     
     func fillCell(dto: BannerCollectionCellDTO) {
+        bannerImage.delegate = self
         bannerImage.downloadImage(withURL: dto.imageURL)
+    }
+    
+    func startLoading() {
+        bannerImage.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+    }
+    
+    func stopLoading() {
+        activityIndicator.stopAnimating()
+        bannerImage.isHidden = false
+    }
+}
+
+extension BannerViewCell: cellDelegate {
+    func startLoad() {
+        DispatchQueue.main.async { [weak self] in
+            self?.startLoad()
+        }
+    }
+    
+    func stopLoad() {
+        DispatchQueue.main.async { [weak self] in
+            self?.stopLoading()
+        }
     }
 }
 
