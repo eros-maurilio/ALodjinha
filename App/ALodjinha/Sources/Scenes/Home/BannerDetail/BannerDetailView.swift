57078455de388d4e8b457d1ce7dfdff6fcb64578
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-21 11:45:50 -0300

    Solves toolbar bug

--- App/ALodjinha/Sources/Scenes/Home/BannerDetail/BannerDetailView.swift
+++ App/ALodjinha/Sources/Scenes/Home/BannerDetail/BannerDetailView.swift
@@ -3,7 +3,8 @@ import WebKit
 
 class BannerDetailView: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
-
+    let website = "github.com"
+    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -12,10 +13,35 @@ class BannerDetailView: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        if let url = URL(string: "https://github.com/eros-maurilio") {
+        
+        if let url = URL(string: "https://" + website + "/eros-maurilio") {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
+            navigationController?.toolbar.tintColor = .purple
         }
+        
+        let back = UIBarButtonItem(barButtonSystemItem: .reply, target: webView, action: #selector(webView.goBack))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+                
+        toolbarItems = [back, spacer, refresh]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isToolbarHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+
+        if let host = url?.host {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    navigationController?.isToolbarHidden = false
+                    return
+                }
+        }
+
+        decisionHandler(.cancel)
     }
 }
